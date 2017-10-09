ba0fb223eed83b5cfba43a685fa4ea205273050d
zi-peng.song <zi-peng.song@hpe.com>
2017-10-09 21:45:53 +0800

    dddd

--- showtime.py
+++ showtime.py
@@ -2,13 +2,13 @@
 '''Take a photo.
 Take a photo using a USB or Raspberry Pi camera.
 '''
-
-import time
-import requests
-
-
-aa={ "timezone":time.tzname, "time":time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime())}
-print( aa)
-s = requests.Session()
-response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()), json=aa)
-print(response)
+#
+# import time
+# import requests
+#
+#
+# aa={ "timezone":time.tzname, "time":time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime())}
+# print( aa)
+# s = requests.Session()
+# response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()), json=aa)
+# print(response)
