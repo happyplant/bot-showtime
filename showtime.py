08c4104fce574458a063ae6ddddecb6dd69ce3e9
zi-peng.song <zi-peng.song@hpe.com>
2017-10-09 21:49:43 +0800

    dddd

--- showtime.py
+++ showtime.py
@@ -3,6 +3,7 @@
 Take a photo using a USB or Raspberry Pi camera.
 '''
 #
+import os
 # import time
 # import requests
 #
@@ -12,3 +13,11 @@ Take a photo using a USB or Raspberry Pi camera.
 # s = requests.Session()
 # response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()), json=aa)
 # print(response)
+
+
+if __name__ == '__main__':
+    try:
+        CAMERA = os.environ['camera']
+    except (KeyError, ValueError):
+        CAMERA = 'USB'  # default camera
+    print(CAMERA)
