9cfcda4414d7c10d3711b0c0bb7fb8bf981d314f
zi-peng.song <zi-peng.song@hpe.com>
2017-10-09 21:31:00 +0800

    ddd

--- showtime.py
+++ showtime.py
@@ -1,9 +1,20 @@
-import time
+import os
 import requests
+import json
+
+headers = {'Authorization': 'Bearer ' + os.environ['API_TOKEN'],
+           'content-type': "application/json"}
+response = requests.get('https://my.farmbot.io/api/points', headers=headers)
+points = response.json()
 
 
-aa={ "timezone":time.tzname, "time":time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime())}
-print( aa)
-s = requests.Session()
-response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()), json=aa)
-print(response)
+
+# import time
+# import requests
+#
+#
+# aa={ "timezone":time.tzname, "time":time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime())}
+# print( aa)
+# s = requests.Session()
+# response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()), json=aa)
+# print(response)
