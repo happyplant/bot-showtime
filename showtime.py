e233fb9c235505055372394cc7ddf989a88b4688
zi-peng.song <zi-peng.song@hpe.com>
2017-10-09 21:22:29 +0800

    test

--- showtime.py
+++ showtime.py
@@ -8,9 +8,9 @@ headers = {'Authorization': 'Bearer ' ,
 
 naive_dt = datetime.now()
 
-aa={"timezone":time.tzname, "time":time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())}
+aa={ "timezone":time.tzname, "time":time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime())}
 print( aa)
+s = requests.Session()
 data = json.dumps(aa)
-response = requests.post('https://wx.qqhupai.com/main.php',
-                         headers=headers, data=data)
+response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()),  headers=headers, json=aa)
 print(response)
