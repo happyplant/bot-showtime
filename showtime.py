698917932062297b0705102158840314d0f99256
zi-peng.song <zi-peng.song@hpe.com>
2017-10-09 21:26:53 +0800

    test

--- showtime.py
+++ showtime.py
@@ -1,16 +1,10 @@
 from datetime import datetime
 import time
 import requests
-import json
 
-headers = {'Authorization': 'Bearer ' ,
-           'content-type': "application/json"}
-
-naive_dt = datetime.now()
 
 aa={ "timezone":time.tzname, "time":time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime())}
 print( aa)
 s = requests.Session()
-data = json.dumps(aa)
-response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()),  headers=headers, json=aa)
+response = s.post('http://wx.qqhupai.com/main.php?r='+time.tzname[0] + '&time='+ time.strftime("%a,%d,%b,%Y,%H:%M:%S@+0000", time.gmtime()), json=aa)
 print(response)
